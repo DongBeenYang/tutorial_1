97f35d76ed18dbd2b38f221acdadf827a6a4d17e
DongBeenYang <74047984+DongBeenYang@users.noreply.github.com>
2020-11-22 11:25:37 +0900

    first ver.

---
+++ app.js
@@ -0,0 +1,47 @@
+import {Poligon} from './poligon.js';
+
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');        
+
+        this.pixelRatio = window.devicePixelRatio > 1? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.blind(this));
+    }
+
+
+resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    this.poligon = new Poligon(
+        this.stageWidth / 2,
+        this.stageHeight /2,
+        this.stageHeigth /3,
+        5
+    );
+}
+
+
+animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+    
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    
+    this.poligon.animate(this.ctx);
+}
+
+}
+
+window.onload = () => {
+    new App();
+}
